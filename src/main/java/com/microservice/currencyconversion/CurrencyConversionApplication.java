b575af3cc4b39a1d41fc14ee13f26e8031995b3f
U122846 <nitish.kashyap.sp@lhind.dlh.de>
2020-09-10 22:17:33 +0200

    Currency Conversion

---
+++ src/main/java/com/microservice/currencyconversion/CurrencyConversionApplication.java
@@ -0,0 +1,26 @@
+package com.microservice.currencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Enable Feign Clients
+ * 
+ * Before we are able to use Feign, we need to enable 
+ * it by using @EnableFeignClients annotation on 
+ * the appropriate package where the client proxies are defined.
+
+ * @author kanja
+ *
+ */
+@SpringBootApplication
+@EnableFeignClients("com.microservice.currencyconversion")
+@EnableDiscoveryClient
+public class CurrencyConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionApplication.class, args);
+	}
+}
